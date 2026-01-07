61fd2b3108baf9a66f7e5e5f98c3f1f1bba793cb
Israr khan <israrkhanlap@gmail.com>
2026-01-07 11:53:12 +0530

    added new video or demos

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ function menuOnClick() {
 window.addEventListener("scroll", function () {
     document.body.classList.remove("menu-active");
 });
+
+const demoBtns = document.querySelectorAll('.demo-btn');
+const demoWrappers = document.querySelectorAll('.demo-wrapper');
+
+demoBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        demoBtns.forEach(b => b.classList.remove('active'));
+        demoWrappers.forEach(w => w.classList.remove('active'));
+
+        btn.classList.add('active');
+        document.getElementById(btn.dataset.target).classList.add('active');
+    });
+});
